ae8ef8ef154e4c9164663ed86e0792fd777734af
annalisawells <annalisawells@gmail.com>
2019-10-21 14:32:06 -0600

    Updated files required to deploy app

---
+++ build/precache-manifest.405818e96e85dfea0b87a8f9a7de37b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b5e054ee3e5a7e1375dfc5a82790d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "006e7953ff07477403cd",
+    "url": "/static/css/main.64df963c.chunk.css"
+  },
+  {
+    "revision": "a137ee50a32861575b53",
+    "url": "/static/js/2.8519ad6c.chunk.js"
+  },
+  {
+    "revision": "006e7953ff07477403cd",
+    "url": "/static/js/main.284f69ea.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
